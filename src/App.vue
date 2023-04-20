34f2aed43457c791f91b712a06c4d5100e8b3913
Cresence <devharris93@gmail.com>
2023-04-20 19:31:56 -0400

    Hello world, for real this time.

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,24 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-  <MainCard />
-</template>
-
-<script>
-import MainCard from './components/MainCard.vue';
-
-export default {
-  name: 'App',
-  components: {
-    MainCard
-  }
-}
+<script setup>
+import NavBarComponent from './components/NavBarComponent.vue';
+import ContentComponent from './components/ContentComponent.vue'
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<template>
+  <header>
+    <NavBarComponent />
+    <div class="main">
+      <ContentComponent />
+    </div>
+  </header>
+
+  <main>
+  </main>
+</template>
+
+<style scoped>
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
 }
+
 </style>
---
+++ src/components/ContentComponent.vue
@@ -0,0 +1,21 @@
+
+<template>
+  <div>
+    <img alt="Vue logo" class="logo" src="../assets/logo.svg" width="125" height="125" />
+    <h3>
+      Hello, world!
+    </h3>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+</style>
